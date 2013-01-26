33b131b035c9067e83bf65e15cdab3338c8749bf
Joel Musheno <joelmusheno@gmail.com>
2013-01-25 21:09:21 -0500

    adding missing files? why is this?

---
+++ AptifyProducts/Attributes/AptifriedEntityAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptifyWebApi.Attributes {
+
+
+    /// <summary>
+    /// This allows us to mark up a model's class declaration with
+    /// the entity that it will transmit data to within Aptify
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    internal class AptifriedEntityAttribute : Attribute {
+        public string Name { get; set; }
+    }
+}
