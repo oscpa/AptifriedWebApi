aa9624d347b71f910b84ec6424129fa318a98d9a
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-03-06 10:34:37 -0500

    adding many more types to persons

---
+++ AptifyProducts/Models/AptifriedMemberClassificationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptifyWebApi.Models {
+	public class AptifriedMemberClassificationType {
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string Description { get; set; }
+		public virtual bool IsActive { get; set; }
+		public virtual string DefaultType { get; set; }
+		public virtual string OldID { get; set; }
+	}
+}
---
+++ AptifyProducts/Models/AptifriedMemberStatusType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptifyWebApi.Models {
+	public class AptifriedMemberStatusType {
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string Description { get; set; }
+		public virtual string OldID { get; set; }
+		public virtual bool IsActive { get; set; }
+		public virtual bool IsBenefitEligible { get; set; }
+		public virtual string DefaultMemberType { get; set; }
+		public virtual string DefaultType { get; set; }
+		public virtual bool IsMember { get; set; }
+		public virtual Guid UniqueID { get; set; }
+	}
+}
