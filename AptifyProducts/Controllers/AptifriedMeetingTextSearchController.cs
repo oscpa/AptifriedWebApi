2b84f747e74ac2d3b0737c73cf95c7df9c1aa8f4
Joel Musheno <joelmusheno@gmail.com>
2013-03-11 17:20:35 -0400

    bug in the full text indexing, was able to test after creating a process flow to dump data into our full text index tables in aptify.

---
+++ AptifyProducts/Controllers/AptifriedMeetingTextSearchController.cs
@@ -0,0 +1,46 @@
+﻿using AptifyWebApi.Dto;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptifyWebApi.Controllers {
+    public class AptifriedMeetingTextSearchController : AptifyEnabledApiController {
+        public AptifriedMeetingTextSearchController(ISession session) : base(session) { }
+
+        public IList<AptifriedMeetingTextSearchResultDto> Get(string search) {
+
+            if (string.IsNullOrEmpty(search))
+                throw new HttpException(500, "Must contain text.");
+
+            return GetSearchResults(search);
+        }
+
+        private IList<AptifriedMeetingTextSearchResultDto> GetSearchResults(string search) {
+
+            string fullTextIndexQuery = "SELECT mt.ID, mt.MeetingTitle, idx.[RANK] " +
+                " FROM freetexttable(idxVwWebSearchIndex, TextContent, :search) idx " +
+                " join dbo.vwWebSearchIndex vw on idx.[KEY] = vw.ID " +
+                " join dbo.vwMeetingsTiny mt on mt.ID = vw.EntityRecordID " +
+                " where EntityID = 980 " +
+                " and mt.StartDate >= GetDate() " +
+                " order by idx.[RANK] desc ";
+            var textSearchResult = session.CreateSQLQuery(fullTextIndexQuery)
+                .SetParameter("search", search)
+                .List();
+
+            // TOODO: try to work in a way of strongly typing a collection
+            var resultingList = new List<AptifriedMeetingTextSearchResultDto>();
+            foreach (object[] item in textSearchResult) {
+                resultingList.Add(new AptifriedMeetingTextSearchResultDto() {
+                    Id = Convert.ToInt32(item[0]),
+                    Name = Convert.ToString(item[1]),
+                    Rank = Convert.ToInt32(item[2])
+                });
+            }
+
+            return resultingList;
+        }
+    }
+}
