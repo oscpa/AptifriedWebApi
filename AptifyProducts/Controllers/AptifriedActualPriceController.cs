b24d80ba06e55858fad6fc8b57b90eaa99c020c0
Joel Musheno <joelmusheno@gmail.com>
2013-03-26 16:17:34 -0400

    removed authorization on the price controller, becuase eventually we may want to allow some sort of guest checkout.

--- AptifyProducts/Controllers/AptifriedActualPriceController.cs
+++ AptifyProducts/Controllers/AptifriedActualPriceController.cs
@@ -8,7 +8,7 @@ using AutoMapper;
 using NHibernate;
 
 namespace AptifyWebApi.Controllers {
-	[System.Web.Http.Authorize]
+	
 	public class AptifriedActualPriceController : AptifyEnabledApiController {
 		public AptifriedActualPriceController(ISession session) : base(session) { }
 
@@ -18,7 +18,13 @@ namespace AptifyWebApi.Controllers {
 				throw new HttpException(500, "Add Request not present.", new ArgumentException("addRequest"));
 
 			var orderObj = (Aptify.Applications.OrderEntry.OrdersEntity) AptifyApp.GetEntityObject("Orders", -1);
-			orderObj.ShipToID = addRequest.Products[0].RegistrantId;
+
+            if (AptifyUser != null) {
+                orderObj.ShipToID = AptifyUser.PersonId;
+            }
+            if (addRequest.Products != null && addRequest.Products.Count > 0) {
+                orderObj.ShipToID = addRequest.Products[0].RegistrantId;
+            }
 
 			foreach (var addProd in addRequest.Products) {
 				orderObj.AddProduct(addProd.ProductId);
