77480c4741d382e04e6cfe7e5d722d8088e47b2d
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-08-16 10:48:39 -0400

    Adding unnecessary line breaks so that I can annotate that this gives us the service component of Store-Pathfix#703.

--- AptifyProducts/Models/AptifriedWebShoppingCartDetailsCount.cs
+++ AptifyProducts/Models/AptifriedWebShoppingCartDetailsCount.cs
@@ -5,8 +5,10 @@ using System.Web;
 
 namespace AptifyWebApi.Models {
 	public class AptifriedWebShoppingCartDetailsCount {
+
 		public virtual int WebUserId { get; set; }
 		public virtual AptifriedWebUser WebUser { get; set; }
 		public virtual int Items { get; set; }
+
 	}
 }
