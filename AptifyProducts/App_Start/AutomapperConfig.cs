0b633efae62e1272e3a748c169118898a2084490
jprystowsky <jprystowsky@ohio-cpa.com>
2013-10-07 14:52:05 -0400

    Adding KnowledgeTrackingType

--- AptifyProducts/App_Start/AutomapperConfig.cs
+++ AptifyProducts/App_Start/AutomapperConfig.cs
@@ -53,6 +53,7 @@ namespace AptifyWebApi.App_Start
 			Mapper.CreateMap<AptifriedKnowledgeCaptureMode, AptifriedKnowledgeCaptureModeDto>();
 			Mapper.CreateMap<AptifriedKnowledgeCategory, AptifriedKnowledgeCategoryDto>();
 			Mapper.CreateMap<AptifriedKnowledgeStatus, AptifriedKnowledgeStatusDto>();
+			Mapper.CreateMap<AptifriedKnowledgeTrackingType, AptifriedKnowledgeTrackingTypeDto>();
 
             Mapper.CreateMap<AptifriedLicenseStatus, AptifriedLicenseStatusDto>();
 
---
+++ AptifyProducts/Models/AptifriedKnowledgeTrackingTypeMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentNHibernate.Mapping;
+
+namespace AptifyWebApi.Models {
+	public class AptifriedKnowledgeTrackingTypeMap : ClassMap<AptifriedKnowledgeTrackingType> {
+		public AptifriedKnowledgeTrackingTypeMap() {
+			Table("vwKnowledgeTrackingTypes");
+
+			Id(x => x.Name);
+
+			Map(x => x.Name);
+			Map(x => x.Description);
+		}
+	}
+}
