a3ab649ee1343ed411fea98d77e10c3b751aad3f
jprystowsky <jprystowsky@ohio-cpa.com>
2013-10-23 15:51:48 -0400

    Adding initial controller for handling password changes

---
+++ AptifyProducts/Controllers/AptifriedPasswordChangeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using AptifyWebApi.Dto;
+using NHibernate;
+
+namespace AptifyWebApi.Controllers {
+	[Authorize]
+	public class AptifriedPasswordChangeController : AptifyEnabledApiController {
+		public AptifriedPasswordChangeController(ISession session) : base(session) { }
+
+		public bool Post(AptifriedWebUserDto userDto) {
+			if (userDto == null) {
+				throw new HttpException(500, "No web user DTO provided.");
+			}
+
+			if (String.IsNullOrEmpty(userDto.Password)) {
+				throw new HttpException(500, "No password given for web user DTO.");
+			}
+
+
+
+			return true;
+		}
+	}
+}
