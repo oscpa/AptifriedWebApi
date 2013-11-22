0b11cd8d75067261e8377e852c1ecebefc23b7fc
jprystowsky <jprystowsky@ohio-cpa.com>
2013-11-22 10:52:12 -0500

    Adding health check endpoint. This shouldn't be so brittle. Hint hint.

---
+++ AptifyProducts/Controllers/AptifriedHealthCheckController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AptifyWebApi.Dto;
+using AptifyWebApi.Models;
+using AutoMapper;
+using NHibernate;
+
+namespace AptifyWebApi.Controllers {
+	public class AptifriedHealthCheckController : AptifyEnabledApiController {
+		public AptifriedHealthCheckController(ISession session) : base(session) { }
+
+		public AptifriedPersonDto Get() {
+			AptifriedPersonDto personDto = null;
+
+			personDto = Mapper.Map<AptifriedPerson, AptifriedPersonDto>(session.QueryOver<AptifriedPerson>().Where(x => x.Id == 31515).SingleOrDefault());
+
+			return personDto;
+		}
+	}
+}
